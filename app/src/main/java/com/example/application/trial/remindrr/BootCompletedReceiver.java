3aee35130f55064b4bd7aa0c1888afaaa516ab85
eeshan1234 <98.eeshan@gmail.com>
2020-01-09 01:04:00 +0530

    First Commit

---
+++ app/src/main/java/com/example/application/trial/remindrr/BootCompletedReceiver.java
@@ -0,0 +1,17 @@
+package com.example.application.trial.remindrr;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+import android.os.Bundle;
+
+public class BootCompletedReceiver extends BroadcastReceiver
+{
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+        // just create AlarmListAdapter and it will load alarms and start them
+        new AlarmListAdapter(context);
+    }
+}
